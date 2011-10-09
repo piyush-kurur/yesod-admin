2eb31364ce2a32d30afe295f547b8334f40c60af
Piyush P Kurur <ppk@cse.iitk.ac.in>
2011-10-10 00:45:36 +0530

    added a skeleton crud module

--- src/Yesod/Admin.hs
+++ src/Yesod/Admin.hs
@@ -5,5 +5,7 @@ The yesod admin module.
 -}
 
 module Yesod.Admin
-       (
+       ( module Yesod.Admin.Crud
        ) where
+
+import Yesod.Admin.Crud
---
+++ src/Yesod/Admin/Crud.hs
@@ -0,0 +1,9 @@
+{-|
+
+This module describes the crud interface to persistent objects.
+
+-}
+
+module Yesod.Admin.Crud
+       (
+       ) where
