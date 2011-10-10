b991a4ab98525426d148515cbfe49e3f7b0e06bd
Piyush P Kurur <ppk@cse.iitk.ac.in>
2011-10-10 12:16:20 +0530

    data type to capture crud operations.

--- src/Yesod/Admin/Crud.hs
+++ src/Yesod/Admin/Crud.hs
@@ -6,8 +6,10 @@ This module describes the crud interface to persistent objects.
 
 module Yesod.Admin.Crud
        ( ListingStyle(..)
+       , CRUD(..)
        ) where
 
+import Yesod
 import Data.Text (Text)
 
 {-|
@@ -26,3 +28,28 @@ data ListingStyle v = ListingStyle
                       -- ^ How to obtain a particular column of an
                       -- object.
      }
+
+{-|
+
+An admin site exposes different crud operations to different users.
+This data type parameterises various crud operations.
+
+-}
+
+data CRUD sub master v = CRUD
+     { listingStyle  :: ListingStyle v
+                     -- ^ How to list it.
+     , listingFilter :: [Filter v]
+                     -- ^ List only those matching this filter.
+     , listingSort   :: [SelectOpt v]
+                     -- ^ How to sort the objects.
+     , listingSize   :: Maybe Int
+                      -- ^ Bound on maximum objects to list.
+     , createForm    :: Form sub master v
+                     -- ^ Form used for creation.
+     , updateForm    :: v -> Form sub master v
+                     -- ^ Form used for modification.
+     }
+
+-- TOTHINK: How should one display values. Tricky part is objects which
+-- have ids in it. Clearly displaying the ids are no fun.
