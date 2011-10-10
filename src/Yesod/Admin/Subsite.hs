36cf23c5066c96ce7ec6fbed31d7c681ae86c33b
Piyush P Kurur <ppk@cse.iitk.ac.in>
2011-10-10 18:07:16 +0530

    routes added

--- src/Yesod/Admin/Subsite.hs
+++ src/Yesod/Admin/Subsite.hs
@@ -9,6 +9,7 @@ This defines the admin subsite data type.
 module Yesod.Admin.Subsite
        ( Admin
        , getAdmin
+       , AdminRoute(..)
        ) where
 
 import Yesod
@@ -22,3 +23,39 @@ data Admin master v = Admin
 
 getAdmin :: Admin master v
 getAdmin = Admin
+
+{-
+
+The route that we want for an admin interfaces is the following
+
+/         list all objects of type v
+/create                    C - creation
+/read/#(Key b v)           R - read the associate object
+/update/#(Key b v)         U - Update the associate object
+/delete/#(Key b v)         D - Delete the associate object
+
+Unfortunately the current subsite generation TH code does not support
+these generic routes hence we have to code these by hand.
+
+-}
+
+-- ^ The data type that captures the admin related routes of an
+-- object.
+
+data AdminRoute backend v = AdminListR          -- ^ Route to list the objects
+                          | AdminCreateR        -- ^ Route to create an object
+                          | AdminReadR   (Key backend v)
+                                                -- ^ Route to view an object
+                          | AdminUpdateR (Key backend v)
+                                                -- ^ Route to update an objects
+                          | AdminDeleteR (Key backend v)
+                                                -- ^ Route to delete an object
+                          deriving Eq
+
+instance SinglePiece (Key backend v)
+         => RenderRoute (AdminRoute backend v) where
+         renderRoute AdminListR       = ([],[])
+         renderRoute AdminCreateR     = (["create"],[])
+         renderRoute (AdminReadR   k) = (["read",   toSinglePiece k],[])
+         renderRoute (AdminUpdateR k) = (["update", toSinglePiece k],[])
+         renderRoute (AdminDeleteR k) = (["delete", toSinglePiece k],[])
