dfbd70221868aa0501eef9a0c71d26afd6a85702
Piyush P Kurur <ppk@cse.iitk.ac.in>
2011-11-17 18:37:59 +0530

    added the listing data type

--- src/Yesod/Admin/Render.hs
+++ src/Yesod/Admin/Render.hs
@@ -1,3 +1,39 @@
+{-# LANGUAGE QuasiQuotes        #-}
+
+{-|
+
+Admin pages have contents that are listings of object, forms to edit
+objects, pages asking for confirmation of certain actions etc. This
+module captures data types that abstract out these pages.  The actual
+rendering of the pages into HTML/CSS is done else where (see the class
+`HasAdminLayout` of "Yesod.Admin.Class"). The advantage of this split
+is that one can change the layout without making any change to the
+rest of the admin code.
+
+-}
 module Yesod.Admin.Render
-       (
+       ( Listing(..)
        ) where
+
+import Data.Text(Text)
+import Yesod
+import Yesod.Admin.Types
+
+-- | Admin pages show the listing of objects. This is the data type
+-- that controls it.
+
+data Listing master = Listing { listingName     :: Text 
+                                      -- ^ Name of the object listed.
+                              , listingHeaders  :: [Text]
+                                      -- ^ The headers of the listing
+                              , listingRows     :: [(Route master, [Text])]
+                                      -- ^ The actual list. The id's
+                                      -- of objects are provied to
+                                      -- facilitate generating links.
+                              , pageNumber      :: Int
+                                      -- ^ Current page.
+                              , perPageCount    :: Int
+                                      -- ^ Total objects per page.
+                              , totalObjects    :: Int
+                                      -- ^ Total number of objects.
+                              }
