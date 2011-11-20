d9aee69f45efe90b9d143034b29e19944ae4f5c6
Piyush P Kurur <ppk@cse.iitk.ac.in>
2011-11-20 14:03:24 +0530

    added the Administrable class

--- src/Yesod/Admin/Class.hs
+++ src/Yesod/Admin/Class.hs
@@ -1,18 +1,64 @@
 {-# LANGUAGE MultiParamTypeClasses     #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE OverloadedStrings         #-}
 {-|
 
 Sites with admin interfaces. 
 
 -}
 module Yesod.Admin.Class
-       ( YesodAdmin(..)
+       ( Administrable(..)
+       , YesodAdmin(..)
        ) where
 
-import Yesod
+import Data.Text(Text, append, pack)
 import Yesod.Auth
-
+import Database.Persist.Base
+import Yesod.Admin.Helpers
 import Yesod.Admin.Subsite
 import Yesod.Admin.Types
+
+-- | This class captures objects that have an admin
+-- interfaces. Minimum complete definition includes the data type
+-- @'Column' v@ and the function 'columnTitle'.
+
+class PersistEntity v => Administrable v where
+
+      -- | The name of the object. Used in various places for example
+      -- in titles of admin pages. The default values is the entity
+      -- of the given persistent type.
+      objectSingular :: v -> Text
+      objectSingular = pack . unCamelCase . entityName . entityDef
+
+      -- | The plural form of the object. By default an `s' is
+      -- appended to the singular form.
+      objectPlural  :: v -> Text
+      objectPlural  v = objectSingular v `append` "s"
+
+      -- | Abstract columns of the type. These columns can appear in
+      -- listings and access control.  Besides the usual database
+      -- columns they may capture other constructed columns.
+      data Column v     :: *
+
+      -- | The title of the given column.
+      columnTitle :: Column v -> Text
+      
+      -- | In listing of the object what all columns should be listed.
+      -- This can be empty in which case the inline display of the
+      -- object is used.
+      listColumns :: [Column v]
+      listColumns = []
+      
+      -- | Controls in which order the objects are listed. By default
+      -- no sorting is done, i.e. it is governed by the database
+      -- backend.
+      listSort   :: [SelectOpt v]
+      listSort = []
+
+      -- | How many elements to be shown on a page. The default value
+      -- is 20. 
+      objectsPerPage :: v -> Int
+      objectsPerPage = const 20
 
 {-|
 
@@ -27,4 +73,3 @@ and hence the type of crudOf is a GHandler.
 -}
 
 class YesodAuth master => YesodAdmin master v where
-
---
+++ src/Yesod/Admin/Helpers.hs
@@ -0,0 +1,22 @@
+module Yesod.Admin.Helpers
+       ( unCamelCase
+       , capitalise
+       , camelCase
+       ) where
+
+
+import Data.Char
+import Data.List
+-- Helper functions
+
+unCamelCase :: String -> String
+unCamelCase = unwords . map (map toLower) . groupBy breaker
+       where breaker x y = not $ isUpper y
+
+capitalise (x:xs) = toUpper x : xs
+
+camelCase :: String -> String
+camelCase str = case wds of
+                     (x:xs) -> concat (x: map capitalise xs)
+                     _      -> str
+     where wds = words str
