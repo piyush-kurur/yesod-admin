27b0f8e2f3472d8bf6d84b432f432619234e6ab7
Piyush P Kurur <ppk@cse.iitk.ac.in>
2011-12-08 12:57:55 +0530

    exposed Y.A.Render and Y.A.TH from Yesod.Admin

--- src/Yesod/Admin.hs
+++ src/Yesod/Admin.hs
@@ -8,8 +8,12 @@ module Yesod.Admin
        ( module Yesod.Admin.Subsite
        , module Yesod.Admin.Class
        , module Yesod.Admin.Types
+       , module Yesod.Admin.Render
+       , module Yesod.Admin.TH
        ) where
 
 import Yesod.Admin.Subsite
 import Yesod.Admin.Class
 import Yesod.Admin.Types
+import Yesod.Admin.Render
+import Yesod.Admin.TH
