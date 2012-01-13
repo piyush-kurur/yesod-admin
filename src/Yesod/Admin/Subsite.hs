5ff143f1fdd2bb789e833e25aabd1c3aa2212751
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-01-13 15:22:54 +0530

    some tweaking of documentation

--- src/Yesod/Admin/Subsite.hs
+++ src/Yesod/Admin/Subsite.hs
@@ -45,12 +45,34 @@ these generic routes hence we have to code these by hand.
 
 {- $routes
 
-In case you want to link to various admin pages you will need to use
-the constructors of this type. Make sure to lift the routes to roots
-of the master site. See the function
-@"Yesod.Admin.Handlers".'toMasterRoute'@
+The Yesod way of capturing Routes is via `Route` type family which for
+the Admin site is instantiated to `AdminRoute`. To point to various
+admin urls, use its constructors. However, you need to lift those to
+the master site.
+
+There are two ways to perform the lift. Consider the first situation
+where you want to link to the admin pages of the Person entity.
+Let the admin route be as follows.
+
+> /admin/person/ PersonAdminR PersonAdmin getPersonAdmin
+
+where @PersonAdmin@ is a type alias @'Admin' Site Person@ and
+getPersonAdmin is the specialised form of @`getAdmin`@. Then the
+constructor @PersonAdminR@ can be used to lift the
+route. I.e. @PersonAdminR `AdminListR`@ is the route to the listing of
+@Persons@, @PersonAdminR $ `AdminReadR` id@ is the route to view the
+person with database id @id@ etc.  
+
+In certain cases the route where the admin is hooked is not known.
+For example all the code in this library does not know how the
+individual admin sites are hooked. Use the combinator
+@"Yesod.Admin.Handlers".'toMasterRoute'@ in such a case.
 
 -}
+
+-- | The routes in the admin site.
+
+type instance Route (Admin master v) = AdminRoute (YesodPersistBackend master) v
 
 -- | The data type that captures the admin related routes of an
 -- object.
@@ -66,6 +88,8 @@ data AdminRoute backend v = AdminListR          -- ^ Route to list the objects.
                                                 -- ^ Route to delete an object
                           deriving (Eq, Show, Read)
 
+
+
 instance SinglePiece (Key backend v)
          => RenderRoute (AdminRoute backend v) where
          renderRoute AdminListR       = ([],[])
@@ -75,4 +99,4 @@ instance SinglePiece (Key backend v)
          renderRoute (AdminUpdateR k) = (["update", toSinglePiece k],[])
          renderRoute (AdminDeleteR k) = (["delete", toSinglePiece k],[])
 
-type instance Route (Admin master v) = AdminRoute (YesodPersistBackend master) v
+
