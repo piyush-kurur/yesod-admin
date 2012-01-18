4288aa98d8176f2d1eedd5cc93d194bba8defe81
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-01-18 11:24:34 +0530

    modified the helper functions to move it to db level

--- src/Yesod/Admin/Handlers/Helpers.hs
+++ src/Yesod/Admin/Handlers/Helpers.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE FlexibleContexts   #-}
 {-
 
 Here we define some helper functions that are required by all
@@ -17,16 +18,30 @@ import Yesod.Admin.Class
 -- | This function checks whether the authenticated user is an admin user. It 
 -- gives the login page if not authenticated and checks admin permissions if
 -- the user is already authenticated.
+
 withAdminUser :: ( HasAdminUser master
                  , HasAdminLayout master
+                 , Monad (YesodDB sub master)
                  )
               => (AuthId master -> GHandler sub master RepHtml) -- the action
               -> GHandler sub master RepHtml
 withAdminUser action = do aid <- requireAuthId
-                          allow <- isAdminUser aid
+                          allow <- runDB $ isAdminUser aid
                           if allow then action aid
                              else do adminPermissionDenied
+runDBWithAuthId :: ( HasAdminUser master
+                   , HasAdminLayout master
+                   , Monad (YesodDB sub master)
+                   )
+                => (AuthId master -> YesodDB sub master a)
+                -> GHandler sub master a
+runDBWithAuthId action = do aid <- requireAuthId
+                            runDB $ do allow <- isAdminUser aid
+                                       if allow then action aid
+                                          else fail "you dont have admin access"
 
+                   
+                
 adminPermissionDenied :: HasAdminLayout master
                       => GHandler sub master RepHtml
 adminPermissionDenied = adminLayout $ addHtml
