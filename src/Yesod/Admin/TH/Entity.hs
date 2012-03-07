852553682a260057e36b9a0903c588bf34c592b4
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-03-07 17:58:22 +0530

    cleaning up the Y.A.TH.Entity module

--- src/Yesod/Admin/TH/Entity.hs
+++ src/Yesod/Admin/TH/Entity.hs
@@ -21,21 +21,14 @@ module Yesod.Admin.TH.Entity
        -- $attributetitleandconstructors
 
        -- * Helper functions.
-       -- $helpers
          AdminInterface(..)
+       , mkAdminClasses
+       -- * Low level Template haskell functions.
+       -- $lowlevel
        , entityDefToInterface
        , deriveAdministrable
        , deriveInlineDisplay
        , deriveAttributeDisplay
-       , mkAdminClasses
-{-
-       , mkYesodAdmin
-       , mkEntityAdmin
-       -- * Low level Template haskell functions.
-       -- $lowlevel
-       , deriveInlineDisplay
-       , deriveAttributeDisplay
--}
        ) where
 
 import Data.Char
@@ -413,171 +406,13 @@ procAdminSection ed = foldl fieldSetter (defaultInterface ed) adminLines
     where adminLines = fromMaybe [] $ M.lookup "Admin" $ entityExtra ed
 
 
-{-
-
-
-
-
-
-
-
-
-
-defAttrListVar :: Name            -- ^ Name of the variable
-               -> Text            -- ^ The entity name
-               -> [Text]          -- ^ The attributes
-               -> DecQ
-
-{-
-
--- | This function is similar to the 'mkYesodAdmin' function but does
--- not derive a 'YesodAdmin' instance for the given entity. Use this
--- function if you want to have a different access control policy than
--- what is provided by the default 'YesodAdmin' instance.
-
-mkEntityAdmin :: PersistEntity v
-              => String        -- ^ Name of the foundation type
-              -> AdminInterface v
-              -> DecsQ
-mkEntityAdmin site ai = do insts <- sequence [ deriveAdministrable' ai
-                                             , deriveInlineDisplay'  site ai
-                                             , deriveAttributeDisplay' site ai
-                                             ]
-                           aliases <- defEntityAliases site entity
-                           return (insts ++ aliases)
-       where entity = typeName $ getObject ai
-
--- | Given the name of the foundation type and admin interface for a
--- persistent type, this function derives all the necessary class
--- instances that are required create the admin site for this type. It
--- also defines the following aliases. If the entity has name @Foo@
--- then it also splices the declarations:
---
--- > type FooAdmin = Admin Site Foo
--- > getFooAdmin :: Site -> FooAdmin
--- > getFooAdmin _ = getAdmin
---
--- The above declaration is to facilitate hooking the admin site of
--- Foo to the main site. Further it also derives the default
--- 'YesodAdmin' instance where only super user has access to the admin
--- facilities. If you want to configure the access controls explicitly
--- then use the 'mkEntityAdmin' function instead and code up the
--- 'YesodAdmin' instance by hand.
-
-mkYesodAdmin :: PersistEntity v
-             => String            -- ^ Name of the foundation type
-             -> AdminInterface v  -- ^ The admin interface
-             -> DecsQ
-mkYesodAdmin site ai = do inst <- mkEntityAdmin site ai
-                          yaInst <- yadminInst
-                          return $ inst ++ [yaInst]
-  where yadminInst = mkInstance [] ''YesodAdmin [siteType, tyType] []
-        siteType   = conT $ mkName site
-        tyType     = conT $ mkName $ typeName $ getObject ai
-
--- $lowlevel
---
--- You will most likely not need these functions but in case you want
--- to have more control on the generated haskell code you can use
--- these.
-
-
-
-
--- | Same as `deriveInlineDisplay` but does not wrap the declaration
--- inside a list. Not very useful in the wild as splicing expects
--- `DecsQ` instead `DecQ` but useful in defining other template
--- haskell function. Currently not exported.
-
-deriveInlineDisplay' :: PersistEntity v
-                     => String
-                     ->AdminInterface v
-                     -> DecQ
-
-deriveAttributeDisplay site = fmap (:[]) . deriveAttributeDisplay' site
-
-
--- | The TH code @defAdmin Site Foo@ generates the following
--- declarations
---
--- > type FooAdmin = Admin Site Foo
--- > getFooAdmin :: Site -> FooAdmin
--- > getFooAdmin _ = getAdmin
---
-
-defEntityAliases :: String -- ^ the site's foundation type
-                 -> String -- ^ the persistent entity
-                 -> DecsQ
-defEntityAliases site entity = sequence [ tySynD alias [] admin
-                                        , sigD funcName funcType
-                                        , funD funcName [body]
-                                        ]
-     where admin       = conT ''Admin `appT` siteTy
-                                      `appT` entityTy
-           alias       = mkName $ entityAdmin entity
-           funcType    = arrowT `appT` siteTy
-                                 `appT` conT alias
-           funcName    = mkName $ getEntityAdmin entity
-           body        = clause [wildP] (normalB  $ varE 'getAdmin) []
-           siteTy    = conT $ mkName site   -- the TH type of site
-           entityTy  = conT $ mkName entity -- the TH type of the entity
-
--- | TH function to generate the definition of members
--- 'objectSingular' and 'objectPlural'.
-singularPlural :: PersistEntity v
-               => AdminInterface v
-               -> [DecQ]
-singularPlural ai = defun 'objectSingular (singular ai) ++
-                       defun 'objectPlural   (plural ai)
-    where   defun _    []  = []
-            defun name str = [funD name $ [rhs str]]
-            rhs str = clause [wildP] (normalB $ litE $ stringL str) []
-
--- | Define the 'listAttribute' member.
-defListAttributes  :: PersistEntity v
-                => v
-                -> [String]
-                -> DecQ
-defListAttributes v ats = valD (varP 'listAttributes) body []
-    where cons = map (conE . mkName) $ map (constructor v) ats
-          body = normalB $ listE cons
-
-
-
-
-
-
-
-
-getObject :: PersistEntity v
-          => AdminInterface v
-          -> v
-getObject _ = undefined
-
-
-isDBAttribute :: String -> Bool
-isDBAttribute = isUpper . head
-
-constructor :: PersistEntity v => v -> String -> String
-constructor v col | isDBAttribute col = capitalise $ camelCase
-                                                $ unwords [ typeName v
-                                                          , col
-                                                          , "Attribute"
-                                                          ]
-                  | otherwise      = capitalise col
-
-
--}
-
--}
-
 textFun :: Name -> Text -> DecQ
 textFun f t = funD f [rhs []]
         where rhs = clause [wildP] $ normalB $ textL t
 
 
-constructorE   :: Text -> Text -> ExpQ
-constructorE e attr = conE $ mkNameT $ constructor e attr
+-- constructorE   :: Text -> Text -> ExpQ
+-- constructorE e attr = conE $ mkNameT $ constructor e attr
 constructorP   :: Text -> Text -> PatQ
 constructorP e attr = conP (mkNameT $ constructor e attr) []
 
