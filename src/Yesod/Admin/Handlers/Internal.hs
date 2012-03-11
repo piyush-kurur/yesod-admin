27de536976ef414c7620836eb1693236a52cf033
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-03-11 14:49:24 +0530

    added skeleton AdminReadR

--- src/Yesod/Admin/Handlers/Internal.hs
+++ src/Yesod/Admin/Handlers/Internal.hs
@@ -1,20 +1,21 @@
 module Yesod.Admin.Handlers.Internal
-       (
+       ( module Yesod.Admin.Handlers.Internal.AdminReadR
+
 {-
         module Yesod.Admin.Handlers.Internal.AdminListR
        , module Yesod.Admin.Handlers.Internal.AdminPageR
        , module Yesod.Admin.Handlers.Internal.AdminCreateR
-       , module Yesod.Admin.Handlers.Internal.AdminReadR
        , module Yesod.Admin.Handlers.Internal.AdminUpdateR
        , module Yesod.Admin.Handlers.Internal.AdminDeleteR
 -}
        ) where
 
+import Yesod.Admin.Handlers.Internal.AdminReadR
+
 {-
 import Yesod.Admin.Handlers.Internal.AdminListR
 import Yesod.Admin.Handlers.Internal.AdminPageR
 import Yesod.Admin.Handlers.Internal.AdminCreateR
-import Yesod.Admin.Handlers.Internal.AdminReadR
 import Yesod.Admin.Handlers.Internal.AdminUpdateR
 import Yesod.Admin.Handlers.Internal.AdminDeleteR
 -}
--- src/Yesod/Admin/Handlers/Internal/AdminReadR.hs
+++ src/Yesod/Admin/Handlers/Internal/AdminReadR.hs
@@ -12,6 +12,7 @@ module Yesod.Admin.Handlers.Internal.AdminReadR
 
 import Yesod
 import Yesod.Admin.Types
+import Yesod.Admin.Handlers.Internal.Helpers
 
 getAdminReadR :: ( Yesod master
                  , YesodPersist master
@@ -19,6 +20,6 @@ getAdminReadR :: ( Yesod master
                  , PathPiece (Key b v)
                  )
               => Key b v  -- ^ The
-              -> AdminHandler master v RepHtml
+              -> CrudHandler master v RepHtml
 getAdminReadR k = defaultLayout $ do
               addHamlet [hamlet|Should display object of Id #{toPathPiece k}|]
