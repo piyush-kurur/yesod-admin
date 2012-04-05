732cc1a6bd0db744bfe8ca4e1af293ce14dc4440
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-04-05 15:24:09 +0530

    renamed ObjectCollection to Collective

--- src/Yesod/Admin/Message.hs
+++ src/Yesod/Admin/Message.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE OverloadedStrings  #-}
 {-|
 
-All the messages used by Yesod admin site. Declare an instance of
-RenderMessage for AdminMessage in your site to get i18n.
+All the messages used by Yesod admin site. Declare `RenderMessage`
+instance for types here to get i18n support.
 
 -}
 
 module Yesod.Admin.Message
        ( AdminMessage(..)
-       , ObjectCollection(..)
-       , defaultRenderObjectCollection
+       , Collective(..)
+       , defaultRenderCollective
        ) where
 
 import Database.Persist.Store
@@ -32,44 +32,44 @@ data AdminMessage = MsgAdminCreate
                   | MsgAdminLogin
                   | MsgAdminLogout
 
--- | Datatype representing a collection of objects. There are four
--- forms: (1) to describe number of objects, for example when one
--- wants to say something like 10 users, (2) the singular form, for
--- example user, (3) The plural form e.g. users and (4) when you want
--- to give an object range, e.g. 1 to 10 of 100 users. Define a
+-- | Datatype representing various collective forms of objects. There
+-- are four forms: (1) to describe number of objects, for example when
+-- one wants to say something like 10 users, (2) the singular form,
+-- for example user, (3) The plural form e.g. users and (4) when you
+-- want to give an object range, e.g. 1 to 10 of 100 users. Define a
 -- @`RenderMessage`@ instance for this type to get i18n support. See
 -- also the function @`defaultRenderObjectSet`@
-data PersistEntity v => ObjectCollection v
-     = Objects Int      -- ^ numeric form. E.g 10 users
-     | ObjectSingular   -- ^ Singular form. E.g user
-     | ObjectPlural     -- ^ Collective form. E.g users
-     | ObjectRange Int Int Int -- ^ E.g. 1 to 20 of 100 users
+data PersistEntity v => Collective v
+     = Collection Int      -- ^ numeric form. E.g 10 users
+     | Singular            -- ^ Singular form. E.g user
+     | Plural              -- ^ Collective form. E.g users
+     | Range Int Int Int   -- ^ E.g. 1 to 20 of 100 users
 
 -- | Use this function to generate a default instance of
 -- @`RenderMessage'@ for an object set.
-defaultRenderObjectCollection :: PersistEntity v
-                              => ObjectCollection v
-                              -> T.Text
-defaultRenderObjectCollection = render undefined
+defaultRenderCollective :: PersistEntity v
+                        => Collective v
+                        -> T.Text
+defaultRenderCollective = render undefined
      where render :: PersistEntity v
                   => v
-                  -> ObjectCollection v
+                  -> Collective v
                   -> T.Text
            render v os = case os of
-                  Objects 1 -> T.unwords [ "1"
-                                         , singular
-                                         ]
-                  Objects n -> T.unwords [ showT n
-                                         , plural
-                                         ]
-                  ObjectSingular    -> singular
-                  ObjectPlural      -> plural
-                  ObjectRange s e t -> T.unwords [ showT s
-                                                 , "to"
-                                                 , showT e
-                                                 , "of"
-                                                 , render v (Objects t)
-                                                 ]
+                  Collection 1 -> T.unwords [ "1"
+                                            , singular
+                                            ]
+                  Collection n -> T.unwords [ showT n
+                                            , plural
+                                            ]
+                  Singular     -> singular
+                  Plural       -> plural
+                  Range s e t  -> T.unwords [ showT s
+                                            , "to"
+                                            , showT e
+                                            , "of"
+                                            , render v (Collection t)
+                                            ]
                where singular = unCamelCase
                               . unHaskellName
                               . entityHaskell
