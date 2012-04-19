e3dc8480e150c194915b6b84d2902fa6d2a6b511
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-04-19 12:23:19 +0530

    added root handler

--- src/Yesod/Admin/Handlers.hs
+++ src/Yesod/Admin/Handlers.hs
@@ -13,9 +13,38 @@ module Yesod.Admin.Handlers
 import Yesod
 import Yesod.Admin.Types
 import Yesod.Admin.Subsite
+                                               
+-- | Default homepage for admin sites.
 
--- | Lifts an admin route to the master route.
+getAdminRootEntry :: ( YesodPersist master
+                     , b ~ YesodPersistBackend master
+                     , m ~ GHandler sub master
+                     , PersistQuery b m
+                     , Administrable v
+                     , RenderMessage master (Collective v)
+                     , RenderMessage master AdminMessage
+                     )
+                   => Route master      -- ^ Selection route
+                   -> Route master      -- ^ Create route
+                   -> v                 -- ^ The entity
+                   -> GWidget sub master ()
 
-toMasterRoute :: Route (Admin master v) -> AdminHandler master v (Route master)
-toMasterRoute r = do lft <- getRouteToMaster
-                     return $ lft r
+getAdminRootEntry selectR createR v =
+       toWidget [hamlet| <div .entity-name>
+                              <a href=@{selectR} _{plural v}
+                         <a href=@{selectR}>
+                            <div .button .create> _{MsgAdminCreate}
+                |]
+     where plural :: PersistEntity e => e -> Collective e
+           plural _ = Plural
+
+-- | This generates an admin page which each entry being the 
+getAdminRootR :: HasAdminLayout master
+              => [GWidget sub master ()]
+              -> GHandler sub master RepHtml
+getAdminRootR ws = adminLayout [whamlet|
+                                  <ul>
+                                      $foreach w <- ws
+                                           <li .entity-item>
+                                               ^{w}
+                               |]
