6a55473cdc2250a9e98abf19929a56b63e1e633a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-04-25 12:58:51 +0530

    modified the default admin homepage

--- src/Yesod/Admin/Handlers.hs
+++ src/Yesod/Admin/Handlers.hs
@@ -14,35 +14,56 @@ import Yesod
 import Yesod.Admin.Types
 import Yesod.Admin.Subsite
                                                
--- | Default homepage for admin sites.
-
-getAdminRootEntry :: ( YesodPersist master
-                     , b ~ YesodPersistBackend master
-                     , m ~ GHandler sub master
-                     , PersistQuery b m
-                     , Administrable v
-                     , RenderMessage master (Collective v)
-                     , RenderMessage master AdminMessage
-                     )
-                   => Route master      -- ^ Selection route
-                   -> Route master      -- ^ Create route
-                   -> v                 -- ^ The entity
-                   -> GWidget sub master ()
-
-getAdminRootEntry selectR createR v =
+-- | Create an entry corresponding to a admin.
+defaultAdminRootEntry :: ( YesodPersist master
+                         , b ~ YesodPersistBackend master
+                         , m ~ GHandler sub master
+                         , PersistQuery b m
+                         , Administrable v
+                         , RenderMessage master (Collective v)
+                         , RenderMessage master AdminMessage
+                         )
+                         => Route master      -- ^ Selection route
+                         -> Route master      -- ^ Create route
+                         -> v                 -- ^ The entity
+                         -> GWidget sub master ()
+defaultAdminRootEntry selectR createR v =
        toWidget [hamlet| <div .entity-name>
                               <a href=@{selectR} _{plural v}
-                         <a href=@{selectR}>
+                         <a href=@{createR}>
                             <div .button .create> _{MsgAdminCreate}
                 |]
      where plural :: PersistEntity e => e -> Collective e
            plural _ = Plural
 
--- | This generates an admin page which each entry being the 
-getAdminRootR :: HasAdminLayout master
+
+-- | Create an entry on the admin page when only crud is supported.
+defaultAdminCrudEntry :: ( YesodPersist master
+                         , b ~ YesodPersistBackend master
+                         , m ~ GHandler sub master
+                         , PersistQuery b m
+                         , Administrable v
+                         , RenderMessage master (Collective v)
+                         , RenderMessage master AdminMessage
+                         )
+                         => Route master      -- ^ Selection route
+                         -> Route master      -- ^ Create route
+                         -> v                 -- ^ The entity
+                         -> GWidget sub master ()
+defaultAdminCrudEntry createR v =
+       toWidget [hamlet| <div .entity-name> _{plural v}
+                         <a href=@{createR}>
+                            <div .button .create> _{MsgAdminCreate}
+                |]
+     where plural :: PersistEntity e => e -> Collective e
+           plural _ = Plural
+
+                              
+-- | Given a set of entries creates an admin page out of this.
+defaultAdminRootPage :: HasAdminLayout master
               => [GWidget sub master ()]
               -> GHandler sub master RepHtml
-getAdminRootR ws = adminLayout [whamlet|
+defaultAdminRootPage ws = adminLayout [whamlet|
                                   <ul>
                                       $foreach w <- ws
                                            <li .entity-item>
