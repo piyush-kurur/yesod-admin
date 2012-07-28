046b17c7ea7a25402090125ef9fc6e754955f680
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-07-28 15:58:25 +0530

    some doc on th

--- src/Yesod/Admin/TH.hs
+++ src/Yesod/Admin/TH.hs
@@ -10,5 +10,20 @@ module Yesod.Admin.TH
        , module Yesod.Admin.TH.Site
        ) where
 
+{-
+
+Developers notes:
+
+The th code is divided as follows
+
+
+[Entity:] Entity specific code goes here. Should have TH code to
+generate all the admin classes and message instances.
+
+[Site:] The code to link the admin sites to the main site.
+
+
+-}
+
 import Yesod.Admin.TH.Entity
 import Yesod.Admin.TH.Site
